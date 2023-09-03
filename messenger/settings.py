a31b8d2bbc921cf9dfbb41a70cfe373d87fa50a0
Leandri Junior <scorpionswagner@gmail.com>
2023-09-03 00:31:36 -0300

    feat(settings): config for deploy

--- messenger/settings.py
+++ messenger/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uf6-t#vues4al6$3^wy!+blyun1*ac(ybskh-u!#v_s*6h--en
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['34.151.240.101']
 
 
 # Application definition
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'messenger.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
+        'NAME': 'messenger',
+        'USER': 'messenger',
         'PASSWORD': '32654808@Aa',
         'HOST': 'db.npfboerqmhcvbjoiibso.supabase.co',
         'PORT': '5432',
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
