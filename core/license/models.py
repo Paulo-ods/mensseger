b46d145799c8161b1cff14b059ca5b49ab85e64a
Leandri Junior <scorpionswagner@gmail.com>
2023-09-13 13:33:19 -0300

    feat(license): app license, models and init

---
+++ core/license/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+import core.models
+
+
+# Create your models here.
+
+class License(core.models.Log):
+    token = models.UUIDField()
+    activate_date = models.DateTimeField()
+    limit_date = models.DateTimeField()
+    category = models.ForeignKey('LicenseCategory', on_delete=models.DO_NOTHING, null=True)
+    active = models.CharField(default=False)
+    payment = models.ForeignKey('LicensePayment', on_delete=models.DO_NOTHING, null=True)
+
+    class Meta:
+        db_table = 'license'
+
+
+class LicenseCategory(core.models.Log):
+    type_name = models.CharField(max_length=255, primary_key=True)
+    description_name = models.CharField(max_length=255, null=True)
+    subcategory = models.ForeignKey('self', on_delete=models.DO_NOTHING, null=True)
+    time = models.IntegerField(null=True)
+    active = models.CharField(default=False)
+
+    class Meta:
+        db_table = 'license_category'
+
+
+class LicensePayment(core.models.Log):
+    class Meta:
+        db_table = 'license_payment'
+
+
+class LicenseModule(core.models.Log):
+    module = models.CharField(max_length=255, primary_key=True)
+    description = models.CharField(max_length=512, null=True)
+    module_parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, null=True)
+
+    class Meta:
+        db_table = 'license_module'
